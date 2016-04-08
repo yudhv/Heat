f859777cf5f7e70bcaf58f64eab9b6641f26c624
Yudhvir Singh <ys13k1994@gmail.com>
2016-04-08 14:06:39 +0530

    chaged to the current ip

--- www/js/services/SocketService.js
+++ www/js/services/SocketService.js
@@ -6,7 +6,7 @@
     function SocketService(socketFactory){
         return socketFactory({
 
-            ioSocket: io.connect('http://122.173.188.182:3000')
+            ioSocket: io.connect('http://122.173.27.194:3000')
 
         });
     }
